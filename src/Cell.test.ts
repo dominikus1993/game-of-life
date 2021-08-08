2f401ae5f61a9f2ae0c7af0f61eb6d4306a8dff9
dominikus1993 <dominikus1910@gmail.com>
2021-08-08 19:21:47 +0200

    small changes

--- src/Cell.test.ts
+++ src/Cell.test.ts
@@ -1,7 +1,42 @@
+import { AliveCell, Cell, DeadCell, isAlive } from './Cell';
 import { isCorrect } from './Coordinate';
+import each from 'jest-each';
 
-test('test correct 2d point22', () => {
-  const coor = { kind: "2d", x: 0, y: 0 };
-  const subject = isCorrect(coor);
-  expect(subject).toBe(true);
+function createCells(alive: number, dead: number): Cell[] {
+    const cells: Cell[] = [];
+    for (let i = 0; i < alive; i++) {
+        cells.push(new AliveCell({ x: i, y: i }));
+    }
+    for (let i = 0; i < dead; i++) {
+        cells.push(new DeadCell({ x: i, y: i }));
+    }
+    return cells;
+}
+
+describe("test check should be alive", () => {
+    each([
+        [1, false],
+        [2, true],
+        [3, true],
+        [4, false],
+    ]).it("when cell has %d alive nieghbours then should be alive: %s", (alive, shouldBeAlive) => {
+        const neigh = createCells(alive, 8 - alive);
+        const cell = new AliveCell({ x: 0, y: 0 });
+        const subject = cell.checkState(neigh);
+        expect(isAlive(subject)).toBe(shouldBeAlive);
+    })
+});
+
+describe("test check should be dead", () => {
+    each([
+        [1, false],
+        [2, false],
+        [3, true],
+        [4, false],
+    ]).it("when cell has %d alive nieghbours then should be dead: %s", (alive, shouldBeAlive) => {
+        const neigh = createCells(alive, 8 - alive);
+        const cell = new DeadCell({ x: 0, y: 0 });
+        const subject = cell.checkState(neigh);
+        expect(isAlive(subject)).toBe(shouldBeAlive);
+    })
 });
--- src/Cell.ts
+++ src/Cell.ts
@@ -1,13 +1,49 @@
 import { Coordinate } from "./Coordinate";
 
 export interface Cell {
-    Coordinate: Coordinate,
-}
-
-export interface DeadCell extends Cell {
+    readonly Coordinate: Coordinate,
+    checkState: (neighbours: Cell[]) => Cell
     
 }
 
-export interface AliveCell extends Cell {
+function isDead(cell: Cell): cell is DeadCell {
+    return cell instanceof DeadCell;
+}
+
+export function isAlive(cell: Cell): cell is AliveCell {
+    return cell instanceof AliveCell;
+}
+
+export class DeadCell implements Cell {
+    readonly Coordinate: Coordinate;
+
+    constructor(coordinate: Coordinate) {
+        this.Coordinate = coordinate;
+    }
+
+    checkState(neighbours: Cell[]): Cell {
+        const aliveCount = neighbours.filter((cell) => isAlive(cell)).length;
+        if(aliveCount === 3) {
+            return new AliveCell(this.Coordinate);
+        }
+        return this;
+    }
+    
+}
+
+export class AliveCell implements Cell {
+    Coordinate: Coordinate;
+
+    constructor(coordinate: Coordinate) {
+        this.Coordinate = coordinate;
+    }
+
+    checkState(neighbours: Cell[]): Cell {
+        const aliveCount = neighbours.filter((cell) => isAlive(cell)).length;
+        if(aliveCount === 2 || aliveCount === 3) {
+            return this;
+        }
+        return new DeadCell(this.Coordinate);
+    }
 
 }
