07d2ac2f1c93791bae6d4dc37b6ac4a581f40597
Dominik.Kotecki <dominikus1910@gmail.com>
2022-05-23 16:23:35 +0000

    .

--- src/GameOfLife.Core/Actors/CellActor.cs
+++ src/GameOfLife.Core/Actors/CellActor.cs
@@ -1,1 +1,20 @@
 namespace GameOfLife.Core.Actors;
+using Akka;
+using Akka.Actor;
+
+public class CellActor : ReceiveActor
+{
+    public CellActor()
+    {
+        Receive<CellActor.CellState>(state =>
+        {
+            Console.WriteLine($"CellActor: {state.X}, {state.Y}");
+        });
+    }
+
+    public class CellState
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
