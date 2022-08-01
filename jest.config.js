4206cda3417bba1cd31f20eb3b4ce6d16eb675ef
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-08-01 15:57:39 +0000

    .

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,5 @@ module.exports = {
     testMatch: ['<rootDir>/**/*.Test.js'],
     coveragePathIgnorePatterns: ['/\.fable/', '/[fF]able.*/', '/node_modules/'],
     testEnvironment: 'node',
-    "globals": {
-      "NODE_ENV": "test"
-    },
-    transform: {},
-    "moduleDirectories": ["node_modules", "src"]
+    transform: {}
   };
