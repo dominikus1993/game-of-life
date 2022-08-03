fe9d88a7fe08cb7113c1411af65de547dab24592
Dominik.Kotecki <dominikus1910@gmail.com>
2022-08-03 15:21:54 +0000

    upgrade mocha version

--- tests/Cell.fs
+++ tests/Cell.fs
@@ -6,36 +6,36 @@ open GameOfLife
 let cell =
     testList "Cell tests" [
         test "Dead name works" {
-            let cellName = Dead({X = 2; Y = 3}) |> Cell.name
+            let cellName = Dead(TwoDimensionCoordinate(2, 3)) |> Cell.name
             Expect.equal cellName "dead" "dead cell"
         }
         test "Alive name works" {
-            let cellName = Alive({X = 2; Y = 3}) |> Cell.name
+            let cellName = Alive(TwoDimensionCoordinate(2, 3)) |> Cell.name
             Expect.equal cellName "alive" "dead cell"
         }
         test "Check state dead cell when new state should be a Alive" {
-            let sampleCoord = {X = 2; Y = 3};
+            let sampleCoord = TwoDimensionCoordinate(2, 3)
             let cell = Dead(sampleCoord)
             let neighbours = [Alive(sampleCoord); Alive(sampleCoord); Alive(sampleCoord);]
             let subject = cell |> Cell.checkState(neighbours)
             Expect.equal subject (Alive(sampleCoord)) "dead cell"
         }
         test "Check state dead cell when new state should be a Dead"  {
-            let sampleCoord = {X = 2; Y = 3};
+            let sampleCoord = TwoDimensionCoordinate(2, 3);
             let cell = Dead(sampleCoord)
             let neighbours = [Alive(sampleCoord); Alive(sampleCoord); Alive(sampleCoord);Alive(sampleCoord)]
             let subject = cell |> Cell.checkState(neighbours)
             Expect.equal subject (Dead(sampleCoord)) "dead cell"
         }
         test "Check state Alive cell when new state should be a Alive" {
-            let sampleCoord = {X = 2; Y = 3};
+            let sampleCoord = TwoDimensionCoordinate(2, 3)
             let cell = Alive(sampleCoord)
             let neighbours = [Alive(sampleCoord); Alive(sampleCoord); Alive(sampleCoord);]
             let subject = cell |> Cell.checkState(neighbours)
             Expect.equal subject (Alive(sampleCoord)) "dead cell"
         }
         test "Check state dead cell when new state should be a Dead"  {
-            let sampleCoord = {X = 2; Y = 3};
+            let sampleCoord = TwoDimensionCoordinate(2, 3)
             let cell = Dead(sampleCoord)
             let neighbours = [Alive(sampleCoord); Alive(sampleCoord); Alive(sampleCoord);Alive(sampleCoord)]
             let subject = cell |> Cell.checkState(neighbours)
