d089bba36d26232aef89df6eba68ea3214a0186a
Dominik.Kotecki <dominikus1910@gmail.com>
2024-05-16 18:22:34 +0200

    fix

--- src/GameOfLife.Core/Cells/Cell.cs
+++ src/GameOfLife.Core/Cells/Cell.cs
@@ -2,15 +2,15 @@ namespace GameOfLife.Core.Cells;
 
 public abstract class Cell
 {
-    
+    public abstract bool IsAlive { get; }
 }
 
 public sealed class AliveCell : Cell
 {
-    
+    public override bool IsAlive => true;
 }
 
 public sealed class DeadCell : Cell
 {
-    
+    public override bool IsAlive => false;
 }
